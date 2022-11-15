d929b8cb62fb77775038e4c9501d0d773ba1fa03
unreal-kz <ukibas.b@gmail.com>
2022-11-15 10:47:28 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gofiber/fiber"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		err := c.SendString("And the API is UP!")
+		return err
+	})
+
+	app.Listen(":3000")
+}
